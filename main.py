d3d8d6188b9c75a8f53d97841183ff153b805560
Craig Schwarzwald <craig.schwarzwald@thalamusgme.com>
2021-01-26 22:34:21 -0500

    Removed the class, added 'global' to any vars getting assigned within the functions.

--- main.py
+++ main.py
@@ -1,111 +1,117 @@
-class EntireGame:
-    lineup = []
-    inning = 1
-    outs = 0
-    firstBase = ""
-    secondBase = ""
-    thirdBase = ""
-    score = 0
-    currentBatter = 0
-    for i in range(0, 9):
-        lineup.append(input("Enter batter name: ").capitalize())
-    print(lineup)
 
-    #### Print Info ####
+lineup = []
+inning = 1
+outs = 0
+firstBase = ""
+secondBase = ""
+thirdBase = ""
+score = 0
+currentBatter = 0
+for i in range(0, 9):
+    lineup.append(input("Enter batter name: ").capitalize())
+print(lineup)
 
-    def printInfo(self):
+#### Print Info ####
 
-        if self.firstBase != "":
-            print(self.firstBase, "is on first")
-        else:
-            print("There is no one on first")
-        if self.secondBase != "":
-            print(self.secondBase, "is on second")
-        else:
-            print("There is no one on second")
-        if self.thirdBase != "":
-            print(self.thirdBase, "is on third")
-        else:
-            print("There is no one on third")
-        print("The score is: " + str(self.score))
-        print("There are", self.outs, "outs")
-        print("The current batter is", self.lineup[self.currentBatter])
-        print("It is currently the", self.inning, "inning")
+def printInfo():
+    #Because no variables are being re/assigned a value in this function, the Python interpretor assumes global by default
+    if firstBase != "":
+        print(firstBase, "is on first")
+    else:
+        print("There is no one on first")
+    if secondBase != "":
+        print(secondBase, "is on second")
+    else:
+        print("There is no one on second")
+    if thirdBase != "":
+        print(thirdBase, "is on third")
+    else:
+        print("There is no one on third")
+    print("The score is: " + str(score))
+    print("There are", outs, "outs")
+    print("The current batter is", lineup[currentBatter])
+    print("It is currently the", inning, "inning")
 
-    #### End Print Info ####
+#### End Print Info ####
 
-    def doSingle(self):
-        if self.thirdBase != "":
-            self.score += 1 # same as score = score + 1
-            self.thirdBase = ""
-        if self.secondBase != "":
-            self.thirdBase = self.secondBase
-            self.secondBase = ""
-        if self.firstBase != "":
-            self.secondBase = self.firstBase
-        self.firstBase = self.lineup[self.currentBatter]
+def doSingle():
+    global thirdBase, secondBase, firstBase, score
+    if thirdBase != "":
+        score += 1 # same as score = score + 1
+        thirdBase = ""
+    if secondBase != "":
+        thirdBase = secondBase
+        secondBase = ""
+    if firstBase != "":
+        secondBase = firstBase
+    firstBase = lineup[currentBatter]
 
-    def doDouble(self):
-        if self.thirdBase != "":
-            self.score += 1
-            self.thirdBase = ""
-        if self.secondBase != "":
-            self.score += 1
-        if self.firstBase != "":
-            self.thirdBase = self.firstBase
-            self.firstBase = ""
-        self.secondBase = self.lineup[self.currentBatter]
+def doDouble():
+    global thirdBase, secondBase, firstBase, score
+    if thirdBase != "":
+        score += 1
+        thirdBase = ""
+    if secondBase != "":
+        score += 1
+    if firstBase != "":
+        thirdBase = firstBase
+        firstBase = ""
+    secondBase = lineup[currentBatter]
 
-    def doTriple(self):
-        if self.thirdBase != "":
-            self.score += 1
-            self.thirdBase = ""
-        if self.secondBase != "":
-            self.score +=1
-            self.secondBase = ""
-        if self.firstBase != "":
-            self.score +=1
-            self.firstBase = ""
-        self.thirdBase = self.lineup[self.currentBatter]
+def doTriple():
+    global thirdBase, secondBase, firstBase, score
+    if thirdBase != "":
+        score += 1
+        thirdBase = ""
+    if secondBase != "":
+        score +=1
+        secondBase = ""
+    if firstBase != "":
+        score +=1
+        firstBase = ""
+    thirdBase = lineup[currentBatter]
 
-    def doHomeRun(self):
-        if self.thirdBase != "":
-            self.score += 1
-            self.thirdBase = ""
-        if self.secondBase != "":
-            self.score += 1
-            self.secondBase = ""
-        if self.firstBase != "":
-            self.score += 1
-            self.firstBase = ""
-        self.score +=1
+def doHomeRun():
+    global thirdBase, secondBase, firstBase, score
+    if thirdBase != "":
+        score += 1
+        thirdBase = ""
+    if secondBase != "":
+        score += 1
+        secondBase = ""
+    if firstBase != "":
+        score += 1
+        firstBase = ""
+    score +=1
 
-    def doOut(self):
-        self.outs += 1
-        if self.outs == 3:
-            self.thirdBase = ""
-            self.secondBase = ""
-            self.firstBase = ""
-            self.inning += 1
-            self.outs = 0
+def doOut():
+    global outs, thirdBase, secondBase, firstBase, inning
+    outs += 1
+    if outs == 3:
+        thirdBase = ""
+        secondBase = ""
+        firstBase = ""
+        inning += 1
+        outs = 0
 
-    def atBat(self):
-        self.printInfo()
-        result = input("What did the batter do?").lower()
-        if result == "s":
-            self.doSingle()
-        elif result == "d":
-            self.doDouble()
-        elif result == "t":
-            self.doTriple()
-        elif result == "hr":
-            self.doHomeRun()
-        else:
-            self.doOut()
-        self.currentBatter += 1
+def atBat():
+    global currentBatter
+    printInfo()
+    result = input("What did the batter do?").lower()
+    if result == "s":
+        doSingle()
+    elif result == "d":
+        doDouble()
+    elif result == "t":
+        doTriple()
+    elif result == "hr":
+        doHomeRun()
+    else:
+        doOut()
+    currentBatter += 1
 
-entireGame = EntireGame()
-entireGame.atBat()
-entireGame.atBat()
-entireGame.atBat()
-entireGame.printInfo()
+
+atBat()
+atBat()
+atBat()
+printInfo()
